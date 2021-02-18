22577595b01bdad9691965a0f6c30e635d17d736
Baptiste Klocker <baptiste.klocker@gmail.com>
2021-02-18 20:02:37 +0100

    finish the front-end setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+
+const { environment } = require('@rails/webpacker')
+
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
