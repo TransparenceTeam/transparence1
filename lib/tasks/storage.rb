0ee7b0d43f1a154a537ea90a6545b0a8d9fca109
victor le fur <victorlefur@hotmail.fr>
2021-03-01 21:21:02 +0100

    table done

--- lib/tasks/storage.rb
+++ lib/tasks/storage.rb
@@ -22,48 +22,177 @@ namespace :db do
 
 
       def user_timeline_endpoint(twitter_username)
-        JSON.parse(@access_token.request(:get, "#{BASE_URI}statuses/user_timeline.json?screen_name=#{twitter_username}&count=2&tweet_mode=extended")
+        JSON.parse(@access_token.request(:get, "#{BASE_URI}statuses/user_timeline.json?screen_name=#{twitter_username}&count=50&tweet_mode=extended")
         .body)
       end
     end
 
-    politicians = Politician.all
-    politicians.each do |politician|
-      puts politician.twitter_username
-      api = ApiTwitter.new
-      puts api.user_timeline_endpoint(politician.twitter_username)
+    #1366313416252620802
 
-      tweets_content = api.user_timeline_endpoint(politician.twitter_username)
+    #politicians = Politician.all
+    #politicians.each do |politician|
+      #puts politician.twitter_username
+    api = ApiTwitter.new
 
-      tweets_content.each do |user|
+    tweets_content = api.user_timeline_endpoint("EmmanuelMacron")
 
-        unless Tweet.exists?(tweet_id: user['id'])
-          new_tweet = Tweet.create!(
-            username: user['user']['screen_name'],
-            content: user['full_text'],
-            hashtag: user['entities']['hashtags'],
-            date: user['created_at'],
-            tweet_id: user['id'],
-            expanded_tweet_url: user['entities']['urls'][0]['expanded_url'],
-            in_reply_to_status: user['in_reply_to_status_id'],
-            user_description: user['user']['description'],
-            expanded_url: user['user']['entities']['url']['urls'][0]['expanded_url'],
-            followers_count: user['user']['followers_count'],
-            friends_count: user['user']['friends_count'],
-            listed_count: user['user']['listed_count'],
-            avatar_url: user['user']['profile_image_url'],
-            avatar_https: user['user']['profile_image_url_https'],
-            lang: user['lang'],
-            location: user['user']['location'],
-            politician_id: politician.id
-          )
-          puts "tweet id #{new_tweet.tweet_id} has been created from #{new_tweet.username} twitter profile"
+    tweets_content.each do |tweet|
+      begin
+        begin
+          puts "--------"
+          puts "____#####____ Tweet with content WITH media and reply WITH media ____#####____"
+          puts "--------"
+          puts tweet['user']['screen_name']
+          puts tweet['full_text']
+          puts tweet['entities']['hashtags']
+          puts tweet['created_at']
+          puts tweet['id']
+          #puts tweet['entities']['urls'][0]['expanded_url']
+          puts tweet['user']['description']
+          puts tweet['user']['followers_count']
+          puts tweet['user']['friends_count']
+          puts tweet['user']['listed_count']
+          puts tweet['user']['profile_image_url']
+          puts tweet['user']['profile_image_url_https']
+          puts tweet['lang']
+          puts tweet['user']['location']
+          #puts politician.id
+          puts tweet['entities']['media'][0]['media_url'][''] # picture
+          puts tweet['extended_entities']['media'][0]['media_url'] #
+          puts tweet['quoted_status']['user']['screen_name']
+          puts tweet['quoted_status']['created_at']
+          puts tweet['quoted_status']['id']
+          puts tweet['quoted_status']['full_text']
+          puts tweet['quoted_status']['user']['location']
+          puts tweet['quoted_status']['entities']['hashtags'][0]['text']
+          puts tweet['quoted_status']['user']['description']
+          puts tweet['quoted_status']['user']['profile_image_url']
+          puts tweet['quoted_status']['user']['profile_image_url_https']
+          puts tweet['quoted_status']['user']['followers_count']
+          puts tweet['quoted_status']['user']['friends_count']
+          puts tweet['quoted_status']['user']['listed_count']
+          puts tweet['quoted_status']['entities']['media'][0]['media_url_https']
+          puts tweet['quoted_status']['extended_entities']['media'][0]['video_info']['variants'][0]['url']
+        rescue
+          puts "--------"
+          puts "____#####____ Tweet with content WITHOUT media and reply WITH media ____#####____"
+          puts "--------"
+          puts tweet['user']['screen_name']
+          puts tweet['full_text']
+          puts tweet['entities']['hashtags']
+          puts tweet['created_at']
+          puts tweet['id']
+          puts tweet['entities']['urls'][0]['expanded_url']
+          puts tweet['user']['description']
+          puts tweet['user']['followers_count']
+          puts tweet['user']['friends_count']
+          puts tweet['user']['listed_count']
+          puts tweet['user']['profile_image_url']
+          puts tweet['user']['profile_image_url_https']
+          puts tweet['lang']
+          puts tweet['user']['location']
+          #puts politician.id,
+          puts tweet['quoted_status']['user']['screen_name']
+          puts tweet['quoted_status']['created_at']
+          puts tweet['quoted_status']['id']
+          puts tweet['quoted_status']['full_text']
+          puts tweet['quoted_status']['user']['location']
+          puts tweet['quoted_status']['entities']['hashtags']
+          puts tweet['quoted_status']['user']['description']
+          puts tweet['quoted_status']['user']['profile_image_url']
+          puts tweet['quoted_status']['user']['profile_image_url_https']
+          puts tweet['quoted_status']['user']['followers_count']
+          puts tweet['quoted_status']['user']['friends_count']
+          puts tweet['quoted_status']['user']['listed_count']
+          puts tweet['quoted_status']['entities']['media'][0]['media_url_https']
+          puts tweet['quoted_status']['extended_entities']['media'][0]['video_info']['variants'][0]['url']
         end
+      rescue
+        begin
+          puts "--------"
+          puts "____#####____ Tweet with content WITHOUT media and retweet WITHOUT media ____#####____"
+          puts "--------"
+          puts tweet['user']['screen_name']
+          puts tweet['full_text']
+          puts tweet['entities']['hashtags']
+          puts tweet['created_at']
+          puts tweet['id']
+          #puts tweet['entities']['urls'][0]['expanded_url']
+          puts tweet['user']['description']
+          puts tweet['user']['followers_count']
+          puts tweet['user']['friends_count']
+          puts tweet['user']['listed_count']
+          puts tweet['user']['profile_image_url']
+          puts tweet['user']['profile_image_url_https']
+          puts tweet['lang']
+          puts tweet['user']['location']
+          #puts politician.id
+          puts tweet['quoted_status']['user']['screen_name']
+          puts tweet['quoted_status']['created_at']
+          puts tweet['quoted_status']['id']
+          puts tweet['quoted_status']['full_text']
+          puts tweet['quoted_status']['user']['location']
+          puts tweet['quoted_status']['entities']['hashtags']
+          puts tweet['quoted_status']['user']['description']
+          puts tweet['quoted_status']['user']['profile_image_url']
+          puts tweet['quoted_status']['user']['profile_image_url_https']
+          puts tweet['quoted_status']['user']['followers_count']
+          puts tweet['quoted_status']['user']['friends_count']
+          puts tweet['quoted_status']['user']['listed_count']
 
+        rescue
+          begin
+            puts "--------"
+            puts "____#####____ Tweet with content WITH media ____#####____"
+            puts "--------"
+            puts tweet['user']['screen_name']
+            puts tweet['full_text']
+            puts tweet['entities']['hashtags']
+            puts tweet['created_at']
+            puts tweet['id']
+            puts tweet['in_reply_to_status_id']
+            puts tweet['user']['description']
+            puts tweet['user']['followers_count']
+            puts tweet['user']['friends_count']
+            puts tweet['user']['listed_count']
+            puts tweet['user']['profile_image_url']
+            puts tweet['user']['profile_image_url_https']
+            puts tweet['lang']
+            puts tweet['user']['location']
+            #puts politician.id
+            puts tweet['entities']['media']['media_url'][''] # picture
+            puts tweet['entities']['media']['video_info']['variants'][0]['url'] # video
+
+          rescue
+            begin
+              puts "--------"
+              puts "____#####____ Tweet WITH content WITHOUT media ____#####____"
+              puts "--------"
+              puts tweet['user']['screen_name']
+              puts tweet['full_text']
+              puts tweet['entities']['hashtags']
+              puts tweet['created_at']
+              puts tweet['id']
+              puts tweet['in_reply_to_status_id']
+              puts tweet['user']['description']
+              puts tweet['user']['followers_count']
+              puts tweet['user']['friends_count']
+              puts tweet['user']['listed_count']
+              puts tweet['user']['profile_image_url']
+              puts tweet['user']['profile_image_url_https']
+              puts tweet['lang']
+              puts tweet['user']['location']
+
+            rescue
+              puts "fuck"
+            end
+          end
+        end
       end
-
     end
   end
 end
 
 #rake db:fetch_api_twitter
+
+
