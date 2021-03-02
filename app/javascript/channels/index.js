69e2059d2cf6cbf99904f104da5940da7153571d
Gaurav Arora <gauravarora.sra@gmail.com>
2021-03-02 20:51:30 +0100

    algolia search select2

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,3 +4,5 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
+
+
--- app/javascript/channels/select2.js
+++ app/javascript/channels/select2.js
@@ -3,6 +3,7 @@ import 'select2';
 
 const initSelect2 = () => {
   $('.select2').select2();
+  console.log("hello");
 };
 
 export { initSelect2 };
