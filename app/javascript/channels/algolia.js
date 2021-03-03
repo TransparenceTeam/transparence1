94d4b573ea9f32be7fecd14107f6903c799bb187
victor le fur <victorlefur@hotmail.fr>
2021-03-03 17:00:22 +0100

    test algolia

--- app/javascript/channels/algolia.js
+++ app/javascript/channels/algolia.js
@@ -1,22 +1,55 @@
 import algoliasearch from "algoliasearch";
 
+const form = document.querySelector("#search")
+
+const appId = document.querySelector("meta[name='algolia-app-id']").content;
+const searchOnlyApiKey = document.querySelector("meta[name='algolia-search-only-api-key']").content;
+const client = algoliasearch(appId, searchOnlyApiKey);
+const index = client.initIndex('ProjectLaw');
+
+const childDom = () => {
+  let insertedContent = document.querySelectorAll(".law-name");
+
+  if (insertedContent) {
+    console.log(insertedContent);
+    insertedContent.forEach((item) =>
+      item.remove()
+    )
+    console.log(insertedContent);
+    //insertedContent.remove();
+    //console.log(insertedContent)
+  }
+
+};
+
+const dom = (option) => {
+
+  const lawContent = `<option class="law-name">${option}</option>`;
+  form.insertAdjacentHTML('beforeend', lawContent);
+
+};
+
+const queryDb = (index, inputField) => {
+  index.search(inputField.value,{ hitsPerPage: 10, page: 0 })
+      .then((content) => {
+        content.hits.forEach((element) =>
+          dom(element.name)
+        );
+      })
+}
+
+
 const algoliaSearch = () => {
   const inputField = document.querySelector("#search");
 
   if (inputField) {
-    const appId = document.querySelector("meta[name='algolia-app-id']").content;
-    const searchOnlyApiKey = document.querySelector("meta[name='algolia-search-only-api-key']").content;
 
-    const client = algoliasearch(appId, searchOnlyApiKey);
-    const index = client.initIndex('ProjectLaw');
+    inputField.addEventListener("input", (event) => {
+      event.preventDefault();
+      //childDom();
+      inputField.innerHTML = "";
+      queryDb(index, inputField);
 
-    inputField.addEventListener("input", () => {
-      index.search(inputField.value,{ hitsPerPage: 10, page: 0 }).then((content) => {
-        content.hits.forEach(element => console.log(element.name));
-        // handle results however you like...
-
-
-      })
     });
   }
 }
