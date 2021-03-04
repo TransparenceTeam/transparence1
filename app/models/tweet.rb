8f0a52a6b6b61040d5413a4ce8c18fe99426e466
Gaurav Arora <gauravarora.sra@gmail.com>
2021-03-04 23:53:06 +0100

    card-tweet-forms

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
   belongs_to :politician
-  has_many :posts, dependent: :destroy
+  has_one :post, dependent: :destroy
   # validates :tweet_id, presence: true, uniqueness: true
 end
