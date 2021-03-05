e475413df05eaa33066ee3937c38fc6a43e1f7b7
Baptiste Klocker <baptiste.klocker@gmail.com>
2021-03-05 17:14:11 +0100

    clean the branch again

--- app/policies/match_policy.rb
+++ app/policies/match_policy.rb
@@ -3,5 +3,22 @@ class MatchPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def new?
+      create?
+    end
+
+    def update?
+      edit?
+    end
+
+    def edit?
+      true
+    end
+
+    def create?
+      true
+      record.user == user || user.is_contributer?
+    end
   end
 end
