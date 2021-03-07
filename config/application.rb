51aab753af006aa237d809b1ea0ea4adab78fa22
Olga Gabbert <olga.gabbert@gmail.com>
2021-03-07 17:48:41 +0100

    changed the navbars and the buttons

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Transparence
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
