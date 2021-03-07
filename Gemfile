d787ebfe3ef29f806e51d10d2139f0a95f2c9c90
Gaurav Arora <gauravarora.sra@gmail.com>
2021-03-07 18:29:21 +0100

    adding the translations for the tweet. need to check the logic works at the time of creation of tweets. Ping me and we can check together

--- Gemfile
+++ Gemfile
@@ -28,9 +28,8 @@ gem 'twitter', '~> 6.2'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'jquery-rails'
 gem 'hogan_assets'
-
 gem "select2-rails"
-
+gem 'faraday'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,27 @@
 class Tweet < ApplicationRecord
+  # require 'httparty'
+  require 'json'
   belongs_to :politician
   has_one :post, dependent: :destroy
   # has_one :match, :through => :post
-  # has_many :matchs, :through => :post
+  # has_many :matches, :through => :post
   # validates :tweet_id, presence: true, uniqueness: true
+  before_save :fetch_translated_tweet
+
+  private
+
+  def fetch_translated_tweet
+
+    con = Faraday.new
+
+    res = con.post do |req|
+        req.url 'https://api.deepl.com/v2/translate?auth_key=54dd8f74-c7e7-2689-51ba-b34c61055eca',
+        req.body = {text: self.content,
+        target_lang: 'EN',
+        source_lang: 'FR'
+       }
+    end
+    self.translatedtext = JSON.parse(res.body)["translations"][1]["text"]
+  end
+
 end
